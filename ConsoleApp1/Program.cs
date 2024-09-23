7c6baa32d785f7bd54b41d5b441069c6d5306805
sgf <yoursgf@126.com>
2024-09-23 11:22:10 +0800

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Js;
+using System.Diagnostics;
+
+namespace ConsoleApp1;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var configWithJs = Configuration.Default
+            .WithJs()
+    //.Without<JsNavigationHandler>()
+    .WithDefaultLoader()
+        //.With(service)
+    .WithConsoleLogger(ctx => new MyConsoleLogger(ctx));
+
+        var contextWithJs = BrowsingContext.New(configWithJs);
+        var source = File.ReadAllText("Redirect.Html");
+        var docOrg = await contextWithJs.OpenAsync(req =>
+        {
+            req.Address(@"http://localhost:8080//");
+            req.Content(source);
+        }).WaitUntilAvailable();
+
+        var docEl = docOrg.DocumentElement;
+        var docHtml = docEl.OuterHtml;
+        var docScript = docEl.InnerHtml;
+
+        var actDoc = contextWithJs.Active;
+
+        var actDocUrl = actDoc.Url;
+        var actDocLocation = actDoc.Location;
+        var docUrl = docOrg.Url;
+        var docLocation = docOrg.Location;
+
+        var locationFromDocScript = docOrg.ExecuteScript("location;");
+        var locationFromActDocScrip = docOrg.ExecuteScript("location;");
+
+        //i hope any of (actDocUrl,actDocLocation,docUrl,docLocation,locationFromDocScript,locationFromActDocScrip)'s value should be is thread-189739-1-1.html?_dsign=9ffd9d54
+        //because if open Redirect.Html in the broswer will redirect to  thread-189739-1-1.html?_dsign=9ffd9d54
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+    }
+}
+
+
+
+
+public class MyConsoleLogger : IConsoleLogger
+{
+    public MyConsoleLogger(IBrowsingContext browsingContext)
+    {
+
+    }
+
+    public void Log(object[] values)
+    {
+        Trace.WriteLine("错误:");
+        Trace.WriteLine(values);
+    }
+}
